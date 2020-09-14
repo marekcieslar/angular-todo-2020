0e44add5bc69573d68b96c3fab942e46d6614eec
Marek <marek.cieslar@gmail.com>
2020-09-14 16:09:13 +0200

    form on material

--- src/app/todo/components/todo-add/todo-add.component.ts
+++ src/app/todo/components/todo-add/todo-add.component.ts
@@ -1,37 +1,46 @@
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 import { TodoAttrs } from './../../todo.interface';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
 import { Priority } from '../../Priority.enum';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
-  styleUrls: ['./todo-add.component.scss']
+  styleUrls: ['./todo-add.component.scss'],
 })
 export class TodoAddComponent {
   @Output() addTodo = new EventEmitter<TodoAttrs>();
 
   form: FormGroup;
+  @ViewChild('formAddTodo') private formAddTodo: NgForm;
 
   priorities = Object.values(Priority);
 
   constructor() {
     this.form = new FormGroup({
       text: new FormControl(null, {
-        validators: [Validators.required]
+        validators: [Validators.required, Validators.minLength(5)],
       }),
       priority: new FormControl(null, {
-        validators: [Validators.required]
-      })
+        validators: [Validators.required],
+      }),
     });
   }
 
-  onSubmit(): void {
-    console.log(this.form.value)
-    if (this.form.valid) {
-      this.addTodo.emit(this.form.value);
-      this.form.reset();
+  keyPress($event: KeyboardEvent): void {
+    if ($event.key === 'Enter') {
+      this.onSubmit();
     }
   }
 
+  onSubmit(): void {
+    console.log(this.form.get('text').validator(this.form.get('text')));
+
+    console.log(this.form.value);
+    if (this.form.valid) {
+      this.addTodo.emit(this.form.value);
+      this.form.reset();
+      this.formAddTodo.resetForm();
+    }
+  }
 }
