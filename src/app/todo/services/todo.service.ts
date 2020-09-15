07a2454eca172b6947092c8043824e279239d8b2
Marek <marek.cieslar@gmail.com>
2020-09-15 05:39:00 +0200

    sort by done and priority

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -2,19 +2,40 @@ import { Todo } from './../../todo';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { TodoAttrs } from '../todo.interface';
+import { Priority } from '../Priority.enum';
 
 @Injectable()
 export class TodoService {
   todos$ = new BehaviorSubject<Todo[]>([]);
   id = 1;
 
-  constructor() {}
+  constructor() {
+    const t1 = this.addTodo({
+      text: 'logger',
+      priority: Priority.b
+    });
+    const t2 = this.addTodo({
+      text: 'sort by done and priority',
+      priority: Priority.a
+    });
+    const t3 = this.addTodo({
+      text: 'update tests',
+      priority: Priority.a
+    });
+    const t4 = this.addTodo({
+      text: 'footer visuals',
+      priority: Priority.c
+    });
 
-  addTodo(todo: TodoAttrs): void {
+    t2.done = true;
+  }
+
+  addTodo(todo: TodoAttrs): Todo {
     console.log('TodoService - add');
 
     const t = new Todo(todo);
     this.todos$.next([...this.todos$.getValue(), t]);
+    return t;
   }
 
   deleteTodo(todo: Todo): void {
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,8 +1,7 @@
 import { TodoAttrs } from './todo.interface';
 import { Todo } from './../todo';
 import { TodoService } from './services/todo.service';
-import { Component, OnInit } from '@angular/core';
-import { Priority } from './Priority.enum';
+import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -10,13 +9,19 @@ import { map } from 'rxjs/operators';
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent {
   todo$ = this.todoService.todos$.pipe(
     map(data => data.sort((a, b) => {
-      if (a.priority < b.priority) {
+      if (a.done === b.done) {
+        if (a.priority === b.priority) {
+          return 0;
+        } else if (a.priority < b.priority) {
+          return -1;
+        } else {
+          return 1;
+        }
+      } else if (a.done < b.done) {
         return -1;
-      } else if (a.priority === b.priority) {
-        return 0;
       } else {
         return 1;
       }
@@ -24,15 +29,6 @@ export class TodoComponent implements OnInit {
   );
 
   constructor(private readonly todoService: TodoService) {}
-
-  ngOnInit(): void {
-    console.log('TodoComponent - on init');
-
-    this.todoService.addTodo({
-      text: 'from todo component',
-      priority: Priority.c,
-    });
-  }
 
   addTodo(todo: TodoAttrs): void {
     this.todoService.addTodo(todo);
